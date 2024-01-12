7ecc94e3bd444ecad120495a49b9e1e8e21c02a6
Vinffs <vin.demiranda@gmail.com>
2024-01-12 19:07:36 +0100

    added the delete function and modal to confirm deletion

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -36,12 +36,20 @@
                         <td>
                             <a class="btn border" href="{{ route('admin.projects.show', $project->id) }}"><i
                                     class="fa-solid fa-circle-info"></i></a>
-                            <a class="btn border"><i class="fa-solid fa-pencil"></i></a>
-                            <a class="btn border"><i class="fa-solid fa-trash-can"></i></a>
+                            <a class="btn border" href="{{ route('admin.projects.edit', $project->id) }}"><i
+                                    class="fa-solid fa-pencil"></i></a>
+                            <form class="d-inline" action="{{ route('admin.projects.destroy', $project->id) }}"
+                                method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="cancel-button btn btn-danger"
+                                    data-item-title="{{ $project->title }}"><i class="fa-solid fa-trash-can"></i></button>
+                            </form>
                         </td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
     </section>
+    @include('partials.modal_delete');
 @endsection
