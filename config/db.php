7d4d4a66e8283f15b308c37eae8597ccc5cbcafe
Vinffs <vin.demiranda@gmail.com>
2024-01-12 16:32:22 +0100

    created db and made first migration

---
+++ config/db.php
@@ -0,0 +1,47 @@
+<?php
+return [
+    'projects' => [
+        [
+            "image" => "css",
+            "title" => "htmlcss-discord",
+            "description" => "A replica of the official Discord website",
+        ],
+        [
+            "image" => "js",
+            "title" => "js-campominato-dom",
+            "description" => "A replica of the famous game 'Minesweeper'",
+        ],
+        [
+            "image" => "css",
+            "title" => "html-css-boolando",
+            "description" => "A blueprint of an e-commerce website",
+        ],
+        [
+            "image" => "css",
+            "title" => "htmlcss-dropbox",
+            "description" => "A replica of the official Dropbox website",
+        ],
+        [
+            "image" => "html",
+            "title" => "Coding-Challenge",
+            "description" => "A first glimpse of coding",
+        ],
+        [
+            "image" => "js",
+            "title" => "vue-boolzapp",
+            "description" => "A Replica of Whatsapp Webapp",
+        ],
+        [
+            "image" => "vue",
+            "title" => "vite-boolflix",
+            "description" => "A Replica of the official Netflix website using Vite",
+        ],
+        [
+            "image" => "laravel",
+            "title" => "laravel-comics-2",
+            "description" => "Creation of a DC Comics replica using Laravel",
+        ],
+
+
+    ]
+];
